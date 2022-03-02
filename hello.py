1568eee32b90aba02e21103913191cb6b414a6fe
glen <glen.farr@gmail.com>
2022-03-02 21:43:41 +1300

    added a template and style sheet

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 #! /usr/bin python3
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "<h2>Hello, cruel, crazy, beautiful world!</h2>"
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
